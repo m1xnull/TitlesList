7b4bbfdfb516282af540bd7d40d00637cf3f70bf
Adam <m1x.vk.null@gmail.com>
2018-07-07 06:55:46 +0300

    statusSearch & stateSearch -> searchStatus

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -6,20 +6,22 @@ import Info from '../Info';
 @inject('titlesStore')
 
 @observer export default class Search extends Component {
+
     setValue = event => {
         this.props.titlesStore.setValue(event.target.value);
     }
 
     handlerValue = event => {
         event.preventDefault();
-        this.props.titlesStore.handlerValue();
+        this.props.titlesStore.fetchItems();
     }
 
     render() {
         const { titlesStore } = this.props;
+
         return (
             <div className={styles.divSearch}>
-                <form onSubmit={titlesStore.fetchItems}>
+                <form onSubmit={this.handlerValue}>
                     <input className={styles.inputSearch}
                         value={titlesStore.searchValue}
                         type="search"
