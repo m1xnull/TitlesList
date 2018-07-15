ff177160c970365708113e79ef8357cb0e12c7b3
Adam <m1x.vk.null@gmail.com>
2018-07-15 07:40:27 +0400

    merge with feature/api

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -1,21 +1,15 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 `;
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 928px;
-    width: 100%;
-    max-width: 765.6px;
-    background: #eee;
-    justify-content: space-between;
-    margin-top: 10px;
-    padding: 4px;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: space-between;
+  background: #eee;
 `;
-
-
